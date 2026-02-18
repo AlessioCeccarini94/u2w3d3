ef685b5e0de062986098e6e76108f64da50b5944
AlessioCeccarini94 <alessioceccarini23@icloud.com>
2026-02-18 16:31:33 +0100

    run COR

--- src/main/java/alessioceccarini/u2w3d3/U2w3d3Application.java
+++ src/main/java/alessioceccarini/u2w3d3/U2w3d3Application.java
@@ -1,5 +1,6 @@
 package alessioceccarini.u2w3d3;
 
+import alessioceccarini.u2w3d3.entitiesCOR.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,18 @@ public class U2w3d3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(U2w3d3Application.class, args);
-	}
 
+		Militare tenente = new Tenente(1000);
+		Militare capitano = new Capitano(2000);
+		Militare maggiore = new Maggiore(3000);
+		Militare colonnello = new Colonnello(4000);
+		Militare generale = new Generale(5000);
+
+		tenente.setNext(capitano);
+		capitano.setNext(maggiore);
+		capitano.setNext(colonnello);
+		capitano.setNext(generale);
+
+		maggiore.filter(tenente);
+	}
 }
